57c061a6058e6cbc04499fc651220f2c9595a43a
KirillYabl <kirill-yablunovskii@mail.ru>
2020-08-02 14:53:56 +0300

    Add image preview

--- blog/admin.py
+++ blog/admin.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
 from blog.models import Post, Tag, Comment
+from django.utils.safestring import mark_safe
 
 admin.site.register(Tag)
 
 
 @admin.register(Post)
 class FlatAdmin(admin.ModelAdmin):
+    fields = ('title', 'slug', 'author', 'tags', 'text', 'image', 'image_preview', 'published_at', 'likes',)
     search_fields = ('title', 'slug', 'author__username',)
     list_display = ('title', 'slug', 'author', 'published_at',)
     list_filter = ('published_at', 'tags__title')
     raw_id_fields = ('author', 'likes', 'tags',)
+    readonly_fields = ('image_preview',)
     date_hierarchy = 'published_at'
     list_per_page = 20
+
+    def image_preview(self, obj):
+        return mark_safe(f'<img src = "{obj.image.url}" height = "200">')
 
 
 @admin.register(Comment)
